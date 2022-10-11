ca28a5395ad5b6d43212d6855d12b2d0c413cd20
Vince Melia <vince@drifta.co.uk>
2022-10-11 12:04:37 +0100

    Empty Maui project.

---
+++ MonkeyFinderApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MonkeyFinderApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MonkeyFinder
+
+https://www.youtube.com/watch?v=DuNLR_NJv8U
